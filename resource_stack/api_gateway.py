bba7e5a9e531fe3520eac2f1b5f734e64a5abd39
Taras Panasiuk <taras.panasiuk@quintagroup.org>
2024-08-17 16:48:14 +0300

    fix(ApiGateway): Use Mock Integration to echo response body for POST method

--- resource_stack/api_gateway.py
+++ resource_stack/api_gateway.py
@@ -5,98 +5,100 @@ from constructs import Construct
 
 from aws_cdk_architecture.settings import Settings
 
+GET_DRAGON_LIST_TEMPLATE = """
+[
+  #if( $input.params('family') == "red" )
+    {
+      "description_str":"Xanya is the fire tribe's banished general. She broke ranks and has been wandering ever since.",
+      "dragon_name_str":"Xanya",
+      "family_str":"red",
+      "location_city_str":"las vegas",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"e clark ave",
+      "location_state_str":"nevada"
+    },
+    {
+      "description_str":"Eislex flies with the fire sprites. He protects them and is their guardian.",
+      "dragon_name_str":"Eislex",
+      "family_str":"red",
+      "location_city_str":"st. cloud",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"breckenridge ave",
+      "location_state_str":"minnesota"
+    }
+  #elseif( $input.params('family') == "blue" )
+    {
+      "description_str":"Protheus is a wise and ancient dragon that serves on the grand council in the sky world. He uses his power to calm those near him.",
+      "dragon_name_str":"Protheus",
+      "family_str":"blue",
+      "location_city_str":"brandon",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"e morgan st",
+      "location_state_str":"florida"
+    }
+  #elseif( $input.params('dragonName') == "Atlas" )
+    {
+      "description_str":"From the northern fire tribe, Atlas was born from the ashes of his fallen father in combat. He is fearless and does not fear battle.",
+      "dragon_name_str":"Atlas",
+      "family_str":"red",
+      "location_city_str":"anchorage",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"w fireweed ln",
+      "location_state_str":"alaska"
+    }
+  #else
+    {
+      "description_str":"From the northern fire tribe, Atlas was born from the ashes of his fallen father in combat. He is fearless and does not fear battle.",
+      "dragon_name_str":"Atlas",
+      "family_str":"red",
+      "location_city_str":"anchorage",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"w fireweed ln",
+      "location_state_str":"alaska"
+    },
+    {
+      "description_str":"Protheus is a wise and ancient dragon that serves on the grand council in the sky world. He uses his power to calm those near him.",
+      "dragon_name_str":"Protheus",
+      "family_str":"blue",
+      "location_city_str":"brandon",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"e morgan st",
+      "location_state_str":"florida"
+    },
+    {
+      "description_str":"Xanya is the fire tribe's banished general. She broke ranks and has been wandering ever since.",
+      "dragon_name_str":"Xanya",
+      "family_str":"red",
+      "location_city_str":"las vegas",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"e clark ave",
+      "location_state_str":"nevada"
+    },
+    {
+      "description_str":"Eislex flies with the fire sprites. He protects them and is their guardian.",
+      "dragon_name_str":"Eislex",
+      "family_str":"red",
+      "location_city_str":"st. cloud",
+      "location_country_str":"usa",
+      "location_neighborhood_str":"breckenridge ave",
+      "location_state_str":"minnesota"
+    }
+  #end
+]
+"""
+
 
 class ApiGatewayConstruct(Construct):
     def __init__(self, scope: Construct, construct_id: str) -> None:
         super().__init__(scope, construct_id)
 
         self.api = aws_apigateway.RestApi(
-            self, Settings.RESOURCE_API_GATEWAY_ID,
+            self,
+            Settings.RESOURCE_API_GATEWAY_ID,
             rest_api_name=Settings.RESOURCE_API_GATEWAY_NAME,
             description="This service serves resources.",
             endpoint_types=[aws_apigateway.EndpointType.REGIONAL],
         )
-        vtl_template = """
-        [
-          #if( $input.params('family') == "red" )
-            {
-              "description_str":"Xanya is the fire tribe's banished general. She broke ranks and has been wandering ever since.",
-              "dragon_name_str":"Xanya",
-              "family_str":"red",
-              "location_city_str":"las vegas",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"e clark ave",
-              "location_state_str":"nevada"
-            },
-            {
-              "description_str":"Eislex flies with the fire sprites. He protects them and is their guardian.",
-              "dragon_name_str":"Eislex",
-              "family_str":"red",
-              "location_city_str":"st. cloud",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"breckenridge ave",
-              "location_state_str":"minnesota"
-            }
-          #elseif( $input.params('family') == "blue" )
-            {
-              "description_str":"Protheus is a wise and ancient dragon that serves on the grand council in the sky world. He uses his power to calm those near him.",
-              "dragon_name_str":"Protheus",
-              "family_str":"blue",
-              "location_city_str":"brandon",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"e morgan st",
-              "location_state_str":"florida"
-            }
-          #elseif( $input.params('dragonName') == "Atlas" )
-            {
-              "description_str":"From the northern fire tribe, Atlas was born from the ashes of his fallen father in combat. He is fearless and does not fear battle.",
-              "dragon_name_str":"Atlas",
-              "family_str":"red",
-              "location_city_str":"anchorage",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"w fireweed ln",
-              "location_state_str":"alaska"
-            }
-          #else
-            {
-              "description_str":"From the northern fire tribe, Atlas was born from the ashes of his fallen father in combat. He is fearless and does not fear battle.",
-              "dragon_name_str":"Atlas",
-              "family_str":"red",
-              "location_city_str":"anchorage",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"w fireweed ln",
-              "location_state_str":"alaska"
-            },
-            {
-              "description_str":"Protheus is a wise and ancient dragon that serves on the grand council in the sky world. He uses his power to calm those near him.",
-              "dragon_name_str":"Protheus",
-              "family_str":"blue",
-              "location_city_str":"brandon",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"e morgan st",
-              "location_state_str":"florida"
-            },
-            {
-              "description_str":"Xanya is the fire tribe's banished general. She broke ranks and has been wandering ever since.",
-              "dragon_name_str":"Xanya",
-              "family_str":"red",
-              "location_city_str":"las vegas",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"e clark ave",
-              "location_state_str":"nevada"
-            },
-            {
-              "description_str":"Eislex flies with the fire sprites. He protects them and is their guardian.",
-              "dragon_name_str":"Eislex",
-              "family_str":"red",
-              "location_city_str":"st. cloud",
-              "location_country_str":"usa",
-              "location_neighborhood_str":"breckenridge ave",
-              "location_state_str":"minnesota"
-            }
-          #end
-        ]
-        """
         dragon_model = self.api.add_model(
             "DragonModel",
             model_name="DragonModel",
@@ -137,7 +139,7 @@ class ApiGatewayConstruct(Construct):
                         type=aws_apigateway.JsonSchemaType.BOOLEAN
                     ),
                 },
-                required=["dragonName", "family", "description"],
+                required=["dragonName", "family", "city"],
             ),
         )
 
@@ -148,7 +150,9 @@ class ApiGatewayConstruct(Construct):
                 integration_responses=[
                     aws_apigateway.IntegrationResponse(
                         status_code="200",
-                        response_templates={"application/json": vtl_template},
+                        response_templates={
+                            "application/json": GET_DRAGON_LIST_TEMPLATE
+                        },
                     )
                 ],
                 request_templates={"application/json": '{"statusCode": 200}'},
@@ -163,23 +167,23 @@ class ApiGatewayConstruct(Construct):
                         status_code="200",
                         response_templates={
                             "application/json": """
+                                #set($body = $context.requestOverride.path.body)
                                 {
-                                  "hello": "world",
-                                  "dragon_name": "$input.path('$.dragonName')",
-                                  "family": "$input.path('$.family')",
-                                  "description": "$input.path('$.description')",
-                                  "city": "$input.path('$.city')",
-                                  "state": "$input.path('$.state')",
-                                  "country": "$input.path('$.country')",
-                                  "neighborhood": "$input.path('$.neighborhood')",
-                                  "reportingPhoneNumber": "$input.path('$.reportingPhoneNumber')",
-                                  "confirmationRequired": "$input.path('$.confirmationRequired')"
+                                    "statusCode": 200,
+                                    "body": $body
                                 }
-                                """,
+                                """
                         },
                     )
                 ],
-                request_templates={"application/json": '{"statusCode": 200}'},
+                request_templates={
+                    "application/json": """
+                    #set($context.requestOverride.path.body = $input.body)
+                    {
+                        "statusCode": 200
+                    }
+                    """
+                },
             ),
             method_responses=[aws_apigateway.MethodResponse(status_code="200")],
             request_validator=aws_apigateway.RequestValidator(
