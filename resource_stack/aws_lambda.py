e19bd40ea1f5836411b33ffcd03d650f0a4abb58
Taras Panasiuk <taras.panasiuk@quintagroup.org>
2024-08-24 21:27:12 +0300

    feat: Add CloudWatch alarms for Lambda function trigger events

--- resource_stack/aws_lambda.py
+++ resource_stack/aws_lambda.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
+    Duration,
+    aws_cloudwatch,
     aws_dynamodb,
     aws_lambda,
+    aws_logs,
 )
 from constructs import Construct
 
@@ -23,6 +26,49 @@ class LambdaConstruct(Construct):
             code=aws_lambda.Code.from_asset("lambda/endpoints/list_dragons/"),
             environment={"TABLE_NAME": table.table_name},
             runtime=aws_lambda.Runtime.PYTHON_3_11,
+        )
+
+        init_duration_metric_filter = aws_logs.MetricFilter(
+            self, "InitDurationMetricFilter",
+            log_group=self.list_dragons.log_group,
+            filter_pattern=aws_logs.FilterPattern.literal(
+                f"REPORT RequestId"
+            ),
+            metric_name="InitDuration",
+            metric_namespace="DragonService",
+            metric_value="$.initDuration",
+        )
+
+        init_duration_metric = aws_cloudwatch.Metric(
+            namespace="DragonService",
+            metric_name="InitDuration",
+            dimensions_map={"FunctionName": self.list_dragons.function_name},
+            period=Duration.minutes(1),
+            statistic="Average",
+            color="#DD0000",
+        )
+        init_duration_metric_alarm = init_duration_metric.create_alarm(
+            self, "InitDurationAlarm",
+            evaluation_periods=1,
+            threshold=500,
+            alarm_description="Alarm if the init duration is over 500ms",
+            alarm_name="InitDurationAlarm",
+            comparison_operator=aws_cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        )
+
+        errors_metric = self.create_dragon.metric_errors(
+            color="#FF0000",
+            label="CreateDragonErrors",
+            period=Duration.minutes(1),
+            statistic="Sum",
+        )
+        errors_metric.create_alarm(
+            self, "CreateDragonErrorsAlarm",
+            evaluation_periods=1,
+            threshold=1,
+            alarm_description="Alarm if there are any errors in the CreateDragon function",
+            alarm_name="CreateDragonErrorsAlarm",
+            comparison_operator=aws_cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
         )
 
     @property
